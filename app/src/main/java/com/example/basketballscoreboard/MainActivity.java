681de0b246ae59d2e96c7f45d21848b1f12b3e25
Ayaan Khan <ayaanalh10@gmail.com>
2019-05-31 15:51:19 +0530

    taking &  shareing a screenshot using implicit intent

--- app/src/main/java/com/example/basketballscoreboard/MainActivity.java
+++ app/src/main/java/com/example/basketballscoreboard/MainActivity.java
@@ -3,18 +3,33 @@ package com.example.basketballscoreboard;
 import android.app.AlertDialog;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tomer.fadingtextview.FadingTextView;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+import static java.lang.System.out;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -33,11 +48,21 @@ public class MainActivity extends AppCompatActivity
         labels = res.getStringArray( R.array.displayTeams ) ;
         aScore=findViewById(R.id.scoreA);
         bScore=findViewById(R.id.scoreB);
-        fadingDispView=findViewById(R.id.teamDisp);
+//        fadingDispView=findViewById(R.id.teamDisp);
         createNotificationChannel();
-        fadingDispView.setTexts(labels);
-        fadingDispView.setTimeout(2500, FadingTextView.MILLISECONDS);
+//        fadingDispView.setTexts(labels);
+//        fadingDispView.setTimeout(2500, FadingTextView.MILLISECONDS);
 
+    }
+
+    public Bitmap getScreenShot()
+    {
+        View rootShareView = getWindow().getDecorView().findViewById(android.R.id.content);
+        rootShareView.getRootView();
+        rootShareView.setDrawingCacheEnabled(true);
+        Bitmap scoreSS = Bitmap.createBitmap(rootShareView.getDrawingCache());
+        rootShareView.setDrawingCacheEnabled(false);
+        return scoreSS;
     }
 
     private void createNotificationChannel() {
@@ -63,11 +88,66 @@ public class MainActivity extends AppCompatActivity
         {
             case R.id.resetMenu : onReset();
             return true;
+            case R.id.ShareSSt  : shareScreenShot();
+            return true;
 
             default:return super.onOptionsItemSelected(item);
         }
 
     }
+
+    private void shareScreenShot()
+    {
+        Bitmap screenShot = getScreenShot();
+        Log.i("Original   dimensions", screenShot.getWidth()+" "+screenShot.getHeight());
+        Toast toast=Toast.makeText(getApplicationContext(),"Sceenshot Taken",Toast.LENGTH_SHORT);
+        toast.show();
+        String fileName = "teamAvsteamB";
+
+        String dirPath = store(screenShot, fileName);
+        File file = new File(dirPath, fileName);
+        shareImage(file);
+    }
+
+
+    public static String  store(Bitmap src, String fileName)
+    {
+
+        String dirPath = Environment.getRootDirectory().getAbsolutePath() + "/Screenshots";
+        File dir = new File(dirPath);
+        if(!dir.exists())
+            dir.mkdirs();
+        File file = new File(dirPath, fileName);
+        try
+        {
+            FileOutputStream fOut = new FileOutputStream(file);
+            src.compress(Bitmap.CompressFormat.PNG, 85, fOut);
+            fOut.flush();
+            fOut.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return dirPath;
+    }
+    private void shareImage(File file){
+        Uri uri = Uri.fromFile(file);
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_SEND);
+        intent.setType("image/*");
+
+        intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "");
+        intent.putExtra(android.content.Intent.EXTRA_TEXT, "");
+        intent.putExtra(Intent.EXTRA_STREAM, uri);
+        try
+        {
+            startActivity(Intent.createChooser(intent, "Share Screenshot"));
+        }
+        catch (ActivityNotFoundException e)
+        {
+            Toast.makeText(getApplicationContext(), "No App Available", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
